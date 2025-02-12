4eb57f3a357e2dac4b60df39a7f791d11b8e37b0
elnarasyazani <elnara.syazani@gmail.com>
2025-02-12 21:56:01 +0700

    push

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,31 @@
+import { auth, signIn, signOut } from "../auth"
+
+
+export default async function Home() {
+  const session = await auth();
+  return (
+   <div className="">
+    {session && (
+      <form
+      action={async () => {
+        "use server"
+        await signOut()
+      }}
+    >
+      <button type="submit" className="bg-ig-red p-2 m-4 rounded-lg"><img src={session.user?.image} />{session.user?.name} Sign Out</button>
+    </form>
+    )}
+
+    {!session && (
+      <form
+      action={async () => {
+        "use server"
+        await signIn()
+      }}
+    >
+      <button type="submit" className="bg-ig-orange p-2 rounded-lg">Sign In</button>
+    </form>
+    )}
+   </div>
+  );
+}
